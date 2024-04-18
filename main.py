215ce1a9340584bc5ce17b2a1a1e7e80cd42957e
Lawrenceeeeeeee <775569157@qq.com>
2024-04-18 15:43:58 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+from src import ModelEstimation as me
+
+
+if __name__ == '__main__':
+    x = np.loadtxt("data/x.txt", delimiter=",")
+    y = np.loadtxt("data/y.txt", delimiter=",")
+    index = np.loadtxt("data/index.txt", delimiter=",", dtype=bool)
+    names = np.loadtxt("data/names.txt", delimiter=",", dtype=str)
+    osr = me.ModelEstimation(x, y, index, names)
+    res = osr.cross_validation(10)
+    print(res)
